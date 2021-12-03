37267a31a58bf3741453bc7246e4b26dcabd4f85
Peter Pullen <plpullen@gmail.com>
2021-12-02 21:23:03 -0500

    fixed the clear button bg color

--- src/Components/Settings/ClearBtn.js
+++ src/Components/Settings/ClearBtn.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ClearBtn() {
   return (
-    <div className="ClearBtn box-border h-8 w-8 p-4 bg-blue-200	rounded-3xl	inline-block m-2 flex justify-center	items-center absolute top-2	right-3	">
+    <div className="ClearBtn box-border h-8 w-8 p-4 bg-green-100	rounded-3xl	inline-block m-2 flex justify-center	items-center absolute top-2	right-3	">
     {/* <div className="ClearBtn box-border h-8 w-8 p-4 border-4 bg-blue-200	rounded-lg	inline-block m-2 flex justify-center	items-center"> */}
     <span className="text-xl "><i class="fas fa-times" style={{color:'white;'}}></i></span>
     </div>
